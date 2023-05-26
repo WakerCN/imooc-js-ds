f9d01fae02788626f0b396e3757442ba9a70ee8a
Waker <1209562577@qq.com>
2023-05-26 15:26:23 +0800

    init

---
+++ leetcode/src/stack/20.有效的括号.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=typescript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  for (let index = 0; index < s.length; index++) {
+    const char = s[index];
+    if (char === "[" || char === "{" || char === "(") {
+      stack.push(char);
+    } else {
+      const popChar = stack.pop();
+      if (
+        !popChar ||
+        (popChar === "(" && char !== ")") ||
+        (popChar === "[" && char !== "]") ||
+        (popChar === "{" && char !== "}")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+// @lc code=end
+
+isValid("{[]()}");
+
+export {};
